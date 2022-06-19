99f26e2d8d5979bbc20e2917839723e0acee68f5
bad-boy-discord <andreyzero123@gmail.com>
2022-06-19 17:53:13 +0300

    clear branch

--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./classes/Moderation";
-export * from "./classes/Logger";
--- src/typings/Logger.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Logger {
-  public tag: string;
-
-  log(message: string): void;
-  warn(message: string): void;
-  error(message: string): void;
-}
